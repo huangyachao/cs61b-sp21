d67a1a00c9eecf2063ac3c297569e59a860749e9
huangyachao <1612929789@qq.com>
2025-11-14 09:56:55 +0800

    Complete GuitarHero

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,47 @@
+package gh2;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class GuitarHero {
+
+    public static void main(String[] args) {
+        /* create two guitar strings, for concert A and C */
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        int size = keyboard.length();
+        GuitarString[] stringArray = new GuitarString[size];
+        for (int i = 0; i < size; i++) {
+            stringArray[i] = new GuitarString(440.0 * Math.pow(2, (i - 24) / 12.0));
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index != -1) {
+                    stringArray[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0.0;
+            for (int i = 0; i < size; i++) {
+                sample += stringArray[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < size; i++) {
+                stringArray[i].tic();
+            }
+        }
+    }
+}
+
